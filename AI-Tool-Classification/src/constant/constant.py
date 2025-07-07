14184dfc59b932a2e3e579ff9859cac76059454b
A-A7med-i <an223602@gmail.com>
2025-07-07 23:28:02 +0300

    Update constant

--- AI-Tool-Classification/src/constant/constant.py
+++ AI-Tool-Classification/src/constant/constant.py
@@ -8,6 +8,8 @@ RAW_DATA = (
 )
 REPORT1 = "/media/ahmed/Data/0ps/AI-Tool-Classification/reports/report_1.txt"
 DATA1 = "/media/ahmed/Data/0ps/AI-Tool-Classification/data/processed/data1.csv"
+SCALER_MODEL = "/media/ahmed/Data/0ps/AI-Tool-Classification/models/scaler.joblib"
+CLUSTER_MODEL = "/media/ahmed/Data/0ps/AI-Tool-Classification/models/cluster.joblib"
 
 
 # Logging configuration
@@ -18,6 +20,13 @@ DATEFMT = "%Y-%m-%d %H:%M:%S"
 
 # Features Engineering
 COMPANY_SIZE_MAP = {"Startup": 50, "SME": 500, "Enterprise": 5000}
+
+NUMERICAL_COL = [
+    "adoption_rate",
+    "daily_active_users",
+    "company_size_numeric",
+    "user_engagement_rate",
+]
 
 FINAL_COLUMNS = [
     "country",
@@ -39,3 +48,6 @@ HEIGHT = 800
 N_CLUSTER = 2
 RANDOM_STATE = 0
 N_INIT = 10
+
+# Train Test Split
+TEST_SIZE = 0.2
